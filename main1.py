73b2fdd4d1921afed9deaee2f2fa648ad23dc2cb
Polox2905 <Pauitr1984@gmail.com>
2024-06-10 14:03:50 +0300

    Add files via upload

---
+++ main1.py
@@ -0,0 +1,18 @@
+a="Hello Paul"
+print(len(a))
+first=5
+second=2
+summ=first+second
+diff=first-second
+print(summ), print(diff)
+third=2
+mean=(first+second+third)/3
+print(mean)
+first_string='Понедельник'
+second_string='Вторник'
+print(first_string + "," + " " +  second_string)
+a=2
+b=3
+c=5
+f=(a*b)+(a*c)
+print((f**3)/2)
