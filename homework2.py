5c1212a41e7963d263cc99ac7a17aa50f7bf12e8
Polox2905 <Pauitr1984@gmail.com>
2024-06-12 14:16:06 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+numbe_of_homework=12
+number_of_hours_spent=1.5
+course_name="Python"
+time_on_one_task=(number_of_hours_spent / numbe_of_homework)
+print(f'Курс: {course_name}, всего задач: {numbe_of_homework}, затрачено часов: {number_of_hours_spent}, среднее время выполнения: {time_on_one_task} часа.')
