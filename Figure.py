2e97223df7e10207e1fd254df8eba02c843bbcb8
Polox2905 <Pauitr1984@gmail.com>
2024-08-04 16:33:26 +0300

    Add files via upload

--- Figure.py
+++ Figure.py
@@ -4,10 +4,12 @@ import math
 class Figure:
     sides_count = 0
     filled = False
+    __sides = []
 
-    def __init__(self, __color=(0, 0, 0), __sides=[]):
-        self.__sides = __sides
+    def __init__(self, __color=(0, 0, 0), *args):
+        self.__sides = [*args]
         self.__color = __color
+
 
     def get_color(self):
         return self.__color
@@ -24,7 +26,10 @@ class Figure:
             0 < side and isinstance(side, int) for side in __sides)
 
     def get_sides(self):
-        return self.__sides
+        if len(self.__sides) != self.sides_count:
+           return [1] * self.sides_count
+        else:
+            return self.__sides
 
     def __len__(self):
         raise NotImplementedError("Perimeter must be implemented by subclass")
@@ -34,13 +39,14 @@ class Figure:
             self.__sides = list(new_sides)
 
 
+
 class Circle(Figure):
     sides_count = 1
 
-    def __init__(self, __color=(0, 0, 0), __sides=[]):
-        super().__init__(__color, __sides)
-        self.side=__sides
-        self.radius = int(__sides) / (2 * math.pi)
+    def __init__(self, __color=(0, 0, 0), *args):
+        super().__init__(__color, *args)
+        self.side=sum(args)
+        self.radius = int(*args) / (2 * math.pi)
 
     def get_square(self):
         return self.radius ** 2 * math.pi
@@ -51,54 +57,16 @@ class Circle(Figure):
 
 
 
-class Cube(Figure):
-    sides_count = 12
-
-    def __init__(self, __color=(0, 0, 0), __sides=[]):
-        super().__init__(__color, __sides)
-        self.side = __sides
-
-    def get_volume(self):
-        return self.side ** 3
-
-    def __len__(self):
-        # Периметр куба
-        return 12 * self.side
-
-
-
-class Triangle(Figure):
-    def __init__(self, position, side1, side2, side3, color=(0, 0, 0)):
-        super().__init__(position, color)
-        self.side1 = side1
-        self.side2 = side2
-        self.side3 = side3
-
-    @property
-    def sides_count(self):
-        return 3
-
-    def get_square(self):
-        s = (self.side1 + self.side2 + self.side3) / 2
-        return math.sqrt(s * (s - self.side1) * (s - self.side2) * (s - self.side3))
-
-    def __len__(self):
-        return self.side1 + self.side2 + self.side3
-
-
-
 circle1 = Circle((200, 200, 100), 10) # (Цвет, стороны)
-cube1 = Cube((222, 35, 130), 6)
+
 
 # Проверка на изменение цветов:
 circle1.set_color(55, 66, 77) # Изменится
 print(circle1.get_color())
-cube1.set_color(300, 70, 15) # Не изменится
-print(cube1.get_color())
+
 
 # Проверка на изменение сторон:
-cube1.set_sides(5, 3, 12, 4, 5) # Не изменится
-print(cube1.get_sides())
+print(circle1.get_sides())
 circle1.set_sides(15) # Изменится
 print(circle1.get_sides())
 
@@ -106,4 +74,3 @@ print(circle1.get_sides())
 print(len(circle1))
 
 # Проверка объёма (куба):
-print(cube1.get_volume())
