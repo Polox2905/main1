df3f631d0f044d619d89af2c3520fe73357f5a0f
Polox2905 <Pauitr1984@gmail.com>
2024-08-04 19:13:46 +0300

    Add files via upload

--- Figure.py
+++ Figure.py
@@ -26,13 +26,15 @@ class Figure:
             0 < side and isinstance(side, int) for side in __sides)
 
     def get_sides(self):
-        if len(self.__sides) != self.sides_count:
+        if len(self.__sides) != self.sides_count and len(self.__sides) > 1:
            return [1] * self.sides_count
+        elif len(self.__sides) != self.sides_count and len(self.__sides) == 1:
+            return self.__sides * self.sides_count
         else:
             return self.__sides
 
     def __len__(self):
-        raise NotImplementedError("Perimeter must be implemented by subclass")
+        return sum(self.__sides)
 
     def set_sides(self, *new_sides):
         if self.__is_valid_sides(*new_sides):
@@ -45,28 +47,51 @@ class Circle(Figure):
 
     def __init__(self, __color=(0, 0, 0), *args):
         super().__init__(__color, *args)
-        self.side=sum(args)
-        self.radius = int(*args) / (2 * math.pi)
+#        self.radius = sum(Figure.__sides()) / (2 * math.pi)
+
+#    def get_square(self):
+#        return self.radius ** 2 * math.pi
+
+
+class Cube(Figure):
+    sides_count = 12
+
+    def __init__(self, __color=(0, 0, 0), *args):
+        super().__init__(__color, *args)
+        self.side = sum(args)
+
+    def get_volume(self):
+        return self.side ** 3
+
+
+
+class Triangle(Figure):
+    sides_count = 3
+
+    def __init__(self, __color=(0, 0, 0), *args):
+        super().__init__(__color, *args)
+
+
 
     def get_square(self):
-        return self.radius ** 2 * math.pi
+        s = (self.side1 + self.side2 + self.side3) / 2
+        return math.sqrt(s * (s - self.side1) * (s - self.side2) * (s - self.side3))
 
-    def __len__(self):
-        # Длина окружности
-        return int(self.side)
 
 
 
 circle1 = Circle((200, 200, 100), 10) # (Цвет, стороны)
-
-
+cube1 = Cube((222, 35, 130), 6)
+triangle1=Triangle((100, 75, 235), 3, 3, 3)
 # Проверка на изменение цветов:
 circle1.set_color(55, 66, 77) # Изменится
 print(circle1.get_color())
-
+cube1.set_color(300, 70, 15) # Не изменится
+print(cube1.get_color())
 
 # Проверка на изменение сторон:
-print(circle1.get_sides())
+cube1.set_sides(5, 3, 12, 4, 5) # Не изменится
+print(cube1.get_sides())
 circle1.set_sides(15) # Изменится
 print(circle1.get_sides())
 
@@ -74,3 +99,9 @@ print(circle1.get_sides())
 print(len(circle1))
 
 # Проверка объёма (куба):
+print(cube1.get_volume())
+
+print(triangle1.get_sides())
+print(len(triangle1))
+
+
