4b1d369c331f7fc22679b57f6937b52e5ef660ae
Polox2905 <Pauitr1984@gmail.com>
2024-08-08 20:56:44 +0300

    Add files via upload

---
+++ homework 7.2.py
@@ -0,0 +1,40 @@
+def custom_write(file_name, strings):
+    f = open(file_name, 'w', encoding='utf-8')
+    strings_positions = {}
+    for i, string in enumerate(strings):
+        start_position = f.tell()
+        f.write(string + '\n')
+        strings_positions[(i + 1, start_position)] = string
+    return strings_positions
+
+
+info = [
+    'Text for tell.',
+    'Используйте кодировку utf-8.',
+    'Because there are 2 languages!',
+    'Спасибо!'
+    ]
+
+result = custom_write('test.txt', info)
+for elem in result.items():
+  print(elem)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
