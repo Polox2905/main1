88d0cc5c1d6619e46712d7853921764f94e6663f
Polox2905 <Pauitr1984@gmail.com>
2024-08-10 18:22:56 +0300

    Add files via upload

---
+++ homework 7.5.py
@@ -0,0 +1,42 @@
+import os
+import time
+
+
+directory = "."
+
+
+
+def walk_directory(directory):
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            filepath = os.path.join(root, file)
+            filetime = os.path.getmtime(filepath)
+            formatted_time = time.strftime("%d.%m.%Y %H:%M", time.gmtime(filetime))
+            filesize = os.path.getsize(filepath)
+            parent_dir = os.path.dirname(filepath)
+
+            print(
+                f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт, Время изменения: {formatted_time}, Родительская директория: {parent_dir}')
+
+
+
+walk_directory(directory)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
