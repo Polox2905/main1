6ab9963749da037387f3cce6f2a9428fad302e7c
Polox2905 <Pauitr1984@gmail.com>
2024-08-25 11:07:53 +0300

    Add files via upload

---
+++ homework 9.7.py
@@ -0,0 +1,39 @@
+def is_prime(function):
+    def wrapper(*args, **kwargs):
+        result = function(*args, **kwargs)
+        if result > 1:
+            for i in range(2, int(result**0.5)+1):
+                if not result % i:
+                    print("Составное")
+                    break
+            else:
+                print("Простое")
+        return result
+    return wrapper
+
+@is_prime
+def sum_three(a, b, c):
+    """Суммирует три числа."""
+    return a + b + c
+
+result = sum_three(2, 3, 6)
+print(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
