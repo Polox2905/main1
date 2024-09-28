af35223f3954c93a597d0cceb4232e52f021a484
Polox2905 <Pauitr1984@gmail.com>
2024-09-28 20:30:10 +0300

    Add files via upload

--- homework 11.3.py
+++ homework 11.3.py
@@ -2,7 +2,7 @@ def introspection_info(obj):
 
     type_of_object = type(obj)
 
-    attributes = dir(obj)
+    attributes = [attributes for attributes in dir(obj) if not callable(getattr(obj, attributes))]
 
     methods = [method for method in dir(obj) if callable(getattr(obj, method))]
 
