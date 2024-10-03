e4c0fdd9ed32e6c2ac470563336c28d48cfd429e
Polox2905 <Pauitr1984@gmail.com>
2024-10-03 19:41:42 +0300

    Add files via upload

--- tests_12_4.py
+++ tests_12_4.py
@@ -1,15 +1,13 @@
 import unittest
 from homework_12_4 import Runner, Tournament
 import logging
-import os
 
-#logging.basicConfig(filename="runner_tests.log", level=logging.DEBUG)
+
 logging.basicConfig(filename="runner_tests.log", encoding="utf-8", filemode="w", level=logging.INFO,
                         format="%(asctime)s | %(levelname)s | %(message)s")
 class RunnerTest(unittest.TestCase):
     def test_walk(self):
         try:
-
             runner = Runner('Alice', speed=-1)
             for _ in range(10):
                 runner.walk()
@@ -21,7 +19,6 @@ class RunnerTest(unittest.TestCase):
 
     def test_run(self):
         try:
-
             runner = Runner(123)
             for _ in range(10):
                 runner.run()
@@ -39,8 +36,6 @@ class RunnerTest(unittest.TestCase):
             bob.run()
         self.assertNotEqual(alice.distance, bob.distance)
 
-if not os.path.exists("runner_tests.log"):
-    print("Файл `runner_tests.log` не найден. Возможно, проблемы с доступом.")
-if __name__ == '__main__':
 
+if __name__ == '__main__':
     unittest.main()
